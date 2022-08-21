269f2d81946b515503f404cded947d6747a42cbe
Hidetaka Okamoto <kokkoku214@gmail.com>
2022-08-21 15:25:24 +0900

    Initial Commit

---
+++ Shared/swift_wordpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_wordpressApp.swift
+//  Shared
+//
+//  Created by 岡本秀高 on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_wordpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
